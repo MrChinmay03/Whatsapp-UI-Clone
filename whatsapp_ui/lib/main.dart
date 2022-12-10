1298929389fe462ea90e091e1be641982c89aaf7
MrChinmay03 <111864031+MrChinmay03@users.noreply.github.com>
2022-12-10 21:39:29 +0530

    Update main.dart

--- whatsapp_ui/lib/main.dart
+++ whatsapp_ui/lib/main.dart
@@ -4,12 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
-
-
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
@@ -24,16 +18,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -54,12 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
